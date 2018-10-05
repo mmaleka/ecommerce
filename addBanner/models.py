dc25c295f86282912c0252dd039ef7d599e40cf8
Mpho Maleka <MMaleka@rdm.local>
2018-10-05 17:14:09 +0200

    Fix for the details images

--- addBanner/models.py
+++ addBanner/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 class AddBanner(models.Model):
     name = models.CharField(max_length=150, db_index=True)
@@ -13,6 +16,19 @@ class AddBanner(models.Model):
         ordering = ('name', )
         verbose_name = 'addbanner'
         verbose_name_plural = 'addbanners'
+
+    def save(self):
+        #Opening the uploaded image
+        im = Image.open(self.image)
+        output = BytesIO()
+        #Resize/modify the image
+        im = im.resize( (500,350) )
+        #after modifications, save it to the output
+        im.save(output, format='JPEG', quality=100)
+        output.seek(0)
+        #change the imagefield value to be the newley modifed image value
+        self.image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.image.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+        super(AddBanner,self).save()
 
     def __str__(self):
         return self.name
--- shop/static/shop/js/shop_JS.js
+++ shop/static/shop/js/shop_JS.js
@@ -9,8 +9,6 @@ try{
       image[ii].style.display='none';
       image[ii].style.display='none';
     };
-
-    console.log(image);
     image[0].style.display='block';
   }
 }
@@ -20,18 +18,23 @@ catch(err){
 
 
 
-ImageSliderList = document.ImageSliderList;
-for (var i_ImageSliderList = 0; i_ImageSliderList < ImageSliderList.length; i_ImageSliderList++) {
-  ImageSliderList[i_ImageSliderList].style.display = 'none';
+try {
+  ImageSliderList = document.ImageSliderList;
+  for (var i_ImageSliderList = 0; i_ImageSliderList < ImageSliderList.length; i_ImageSliderList++) {
+    ImageSliderList[i_ImageSliderList].style.display = 'none';
+  }
+
+  var i_image = 0;
+  var images = [];
+  var time = 3000;
+
+  // Image list
+  images[0] = ImageSliderList[0].src;
+  images[1] = ImageSliderList[1].src;
+  images[2] = ImageSliderList[2].src;
+} catch (e) {
+  console.log("Error", e);
 }
-
-var i_image = 0;
-var images = [];
-var time = 3000;
-
-// Image list
-images[0] = ImageSliderList[0].src;
-images[1] = ImageSliderList[1].src;
 
 // Change Image
 
@@ -46,4 +49,30 @@ function changeImg() {
   setTimeout("changeImg()", time);
 }
 
+
+function changeImage(image_id) {
+
+  // Change images on the detail page
+  ImageDetailList = document.ImageDetailList;
+  var images_detailList1ID = document.getElementById(image_id)
+  document.getElementById("mainImage").src = images_detailList1ID.src;
+
+  }
+
 window.onload = changeImg;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
